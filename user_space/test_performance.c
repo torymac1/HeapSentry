f5c732a58ab77901538ad41d4fd1dbbf82d6eded
IvanZ123 <yifanzhai.fall.2018@gmail.com>
2019-05-02 00:23:56 -0400

    add test for mount

--- user_space/test_performance.c
+++ user_space/test_performance.c
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <errno.h>\
+#include <sys/mount.h>
 
 
 void performance(int n)
@@ -85,15 +87,44 @@ int execve_test()
  	return 1;
 }
 
+//https://www.linuxquestions.org/questions/programming-9/how-to-use-mount-function-from-c-920210/
+int mount_test()
+{
+	char *p1 = malloc(10);
+	strcpy(p1,"overflowtesting");
+	const char* src  = "none";
+	const char* trgt = "/var/tmp";
+	const char* type = "tmpfs";
+	const unsigned long mntflags = 0;
+	const char* opts = "mode=7777,uid=1000";   /* 65534 is the uid of nobody */
+	int result = mount(src, trgt, type, mntflags, opts);
+	if (result == 0)
+	{
+	    printf("Mount created at %s...\n", trgt);
+	    printf("Press <return> to unmount the volume: ");
+	    getchar();
+	    umount(trgt);
+	}
+	else
+	{
+	    printf("Error : Failed to mount %s\n"
+	           "Reason: %s [%d]\n",
+	           src, strerror(errno), errno);
+	    return -1;
+	}
+	free(p1);
+	return 0;
+}
+
 int main(){
 	
 	syscall(360);
 
 	//performance test 
-	performance(1000);
+	// performance(1000);
 
 	//overflow with free
-	
+
 	// overflow();
 
 	//overflow test with open, write test
@@ -110,7 +141,10 @@ int main(){
 
     //execve
 
-    execve_test();
+    // execve_test();
+
+    //mount
+    mount_test();
 
 	return 0;
 }
